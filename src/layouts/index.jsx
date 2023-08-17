4e97c1f4255d0f915708f655c7963d89019857e9
coder <liulin_suyan@163.com>
2023-08-17 14:20:55 +0800

    fix: 完善菜单

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Layout, Menu } from "antd";
 import styles from "./index.module.less";
+import { Link } from "react-router-dom";
 
 const { Content, Sider } = Layout;
 const BasicLayout = () => {
   const [selectKeys, setSelectKeys] = useState([]);
   const list = [
-    { label: "第一题", key: "/first" },
-    { label: "第二题", key: "/second" },
-    { label: "第三题", key: "/third" },
+    { label: <Link to="/first">第一题</Link>, key: "/first" },
+    { label: <Link to="/second">第二题</Link>, key: "/second" },
+    { label: <Link to="/third">第三题</Link>, key: "/third" },
   ];
   return (
     <Layout className={styles.layout}>
-      <Sider>
+      <Sider theme="light">
         <Menu
           selectedKeys={selectKeys}
+          theme="light"
           mode="inline"
           items={list}
           onSelect={({ selectedKeys }) => setSelectKeys(selectedKeys)}
